8f1ca82c73924f15e08fd7b4f95ceb035d5f2b02
hmyo2853 <hmyo2853@gmail.com>
2024-03-31 22:05:01 +0900

    css: 메인 PC 버전 레이아웃 UI 잡기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import profileImg from "../images/profile.webp";
 
 export default function Home() {
   return (
@@ -14,11 +15,21 @@ export default function Home() {
         {/* 프로필 사진 및 gelee 정보 */}
         <div className={styles.profile}>
           <p>Profile</p>
-          <Image src="" alt="gelog 프로필 이미지" width={200} height={200} />
+          <Image
+            src={profileImg}
+            alt="gelog 프로필 이미지"
+            width={200}
+            height={200}
+          />
+          <div>
+            <p>Github</p>
+            <p>Email</p>
+            <p>LinkedIn</p>
+          </div>
         </div>
         <div>
           {/* 사용한 블로그 태그들 */}
-          <div>
+          <div className={styles.tags}>
             <p>NextJs</p>
             <p>React</p>
             <p>Front-end</p>
@@ -32,17 +43,54 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h2>
-                블로그 글 1 <span>-&gt;</span>
-              </h2>
+              <div className={styles.thumnail}>
+                <h2>
+                  NextJs 14로 블로그 만들기 <span>-&gt;</span>
+                </h2>
+              </div>
               <p>
-                Instantly deploy your Next.js site to a shareable URL with
-                Vercel.
+                2024년 NextJs는 어떻게 바뀌었을까? 주니어 프론트엔드의 우당탕탕
+                NextJs 14 블로그 만들기 기록
+              </p>
+            </a>
+
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className={styles.thumnail}>
+                <h2>
+                  NextJs 14로 블로그 만들기 <span>-&gt;</span>
+                </h2>
+              </div>
+              <p>
+                2024년 NextJs는 어떻게 바뀌었을까? 주니어 프론트엔드의 우당탕탕
+                NextJs 14 블로그 만들기 기록
+              </p>
+            </a>
+
+            <a
+              href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+              className={styles.card}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className={styles.thumnail}>
+                <h2>
+                  NextJs 14로 블로그 만들기 <span>-&gt;</span>
+                </h2>
+              </div>
+              <p>
+                2024년 NextJs는 어떻게 바뀌었을까? 주니어 프론트엔드의 우당탕탕
+                NextJs 14 블로그 만들기 기록
               </p>
             </a>
           </div>
         </div>
       </div>
+      <footer className={styles.footer}>© Ga Eun Lee 2024</footer>
     </main>
   );
 }
